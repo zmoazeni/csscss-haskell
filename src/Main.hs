9ed2939f6406cc4924cbc4c1a6bcc09bbbb2f389
Zach Moazeni <zach@collectiveidea.com>
2012-08-09 21:17:56 -0400

    Simple parsing from command line

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,20 @@
 module Main where
 
 import System.Environment (getArgs)
+import Text.CSS.Parse
+import Data.Text hiding (head)
 
 main :: IO ()
 main = do
   args <- getArgs
-  showFile args
-  where showFile [] = putStrLn "Need a file to parse"
+  parseFile args
+  where showFile [] = printError
         showFile (filePath:_) = readFile filePath >>= putStr
+        
+        parseFile [] = printError
+        parseFile (filePath:_) = do contents <- readFile filePath
+                                    let Right rules = parseBlocks (pack contents)
+                                    print $ head rules
+          
+        printError = putStrLn "Need a file to parse"
         
