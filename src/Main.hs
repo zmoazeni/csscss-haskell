4c3deebc3230f7d0747d74decff4f1bd6a4e947f
Zach Moazeni <zach.moazeni@gmail.com>
2012-08-10 23:04:36 -0400

    Adds some Ruleset and Rule types and printing the first 3

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,9 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import System.Environment (getArgs)
 import Text.CSS.Parse
-import Data.Text hiding (head)
+import Data.Text (Text, pack)
 
 main :: IO ()
 main = do
@@ -13,8 +14,19 @@ main = do
         
         parseFile [] = printError
         parseFile (filePath:_) = do contents <- readFile filePath
-                                    let Right rules = parseBlocks (pack contents)
-                                    print $ head rules
-          
-        printError = putStrLn "Need a file to parse"
+                                    either printParseError displayRulesets $ parseBlocks (pack contents)
+                                    
+        displayRulesets rawRulesets = do let rulesets = map buildRuleset rawRulesets
+                                         print $ take 3 rulesets
+        buildRulesets = map buildRuleset
+        buildRuleset (selector, rawRules) = Ruleset selector (buildRules rawRules)
+        buildRules = map (uncurry Rule)
         
+        printError = putStrLn "Need a file to parse"
+        printParseError error = putStrLn $ "Error parsing css: " ++ error
+
+data Rule = Rule {property :: Text, value :: Text}
+            deriving (Show)
+                     
+data Ruleset = Ruleset {selector :: Text, rules :: [Rule]}
+             deriving (Show)
