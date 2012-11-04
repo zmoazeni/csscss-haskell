70b34809db1548dfb873aa3ee264c4ffdfa65ef5
Zach Moazeni <zach.moazeni@gmail.com>
2012-11-04 07:18:48 -0500

    Whitespace

--- src/Main.hs
+++ src/Main.hs
@@ -12,11 +12,11 @@ main = do
   parseFile args
   where showFile [] = printError
         showFile (filePath:_) = readFile filePath >>= putStr
-        
+
         parseFile [] = printError
         parseFile (filePath:_) = do contents <- readFile filePath
                                     either printParseError displayRulesets $ parseBlocks (pack contents)
-                                    
+
         displayRulesets rawRulesets = do let rulesets = map buildRuleset rawRulesets
                                          print $ take 3 rulesets
         printError = putStrLn "Need a file to parse"
