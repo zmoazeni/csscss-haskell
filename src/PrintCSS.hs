b35b7f54d522b5f2ed2d391995c7a0f57bc748be
Zach Moazeni <zach.moazeni@gmail.com>
2012-11-10 01:34:11 -0500

    Adds option parser and options

--- src/PrintCSS.hs
+++ src/PrintCSS.hs
@@ -1,42 +1,88 @@
 {-# LANGUAGE OverloadedStrings #-}
 module PrintCSS where
 
-import System.Environment (getArgs)
+import System.Environment (getArgs, getProgName)
+import System.Exit
 import Text.CSS.Parse
 import Data.Text (Text, pack, unpack)
 import Text.PrettyPrint
 import Text.Printf
+import System.Console.GetOpt
+import Data.Maybe (fromMaybe, fromJust)
+import Control.Monad
 
 import Rulesets
 import RedundancyCalc
 
+data Options = Options
+ { optVerbose     :: Bool
+ , optShowVersion :: Bool
+ , optShowHelp    :: Bool
+ , optNum         :: Maybe Int
+ } deriving Show
+
+defaultOptions    = Options
+ { optVerbose     = False
+ , optShowVersion = False
+ , optShowHelp    = False
+ , optNum         = Just 3
+ }
+
+options :: [OptDescr (Options -> Options)]
+options =
+ [ Option ['v'] ["verbose"]
+     (NoArg (\ opts -> opts { optVerbose = True }))
+     "Print each shared rule."
+ , Option ['V'] ["version"]
+     (NoArg (\ opts -> opts { optShowVersion = True }))
+     "Show version number."
+ , Option ['n'] ["num"]
+     (OptArg ((\num opts -> opts { optNum = Just num }) . read . fromMaybe "3") "NUM")
+     "Print selectors that match at least NUM times. Defaults 3."
+ , Option ['h', 'H'] ["help"]
+     (NoArg (\ opts -> opts { optShowHelp = True }))
+     "Display this help message."
+ ]
+
+parseOpts :: String -> [String] -> IO (Options, [String])
+parseOpts progName argv =
+   case getOpt Permute options argv of
+      (o,n,[]  ) -> return (foldl (flip id) defaultOptions o, n)
+      (_,_,errs) -> do
+        putStr $ concat errs
+        printHelp progName
+
+printHelp progName = do
+  putStr (usageInfo header options)
+  exitWith ExitSuccess
+  where
+    header = "Usage: " ++ progName ++ " [OPTION...] cssfile"
+
 main :: IO ()
 main = do
-  args <- getArgs
-  if (null args) then printError
+  progName <- getProgName
+  (args, files) <- getArgs >>= parseOpts progName
+  if (optShowHelp args) || null files
+    then printHelp progName
     else do
-      errorOrRules <- parseFile args
+      errorOrRules <- parseFile files
       case errorOrRules of
         Left e   -> printParseError e
-        Right rs -> putStrLn $ render (displayRulesets rs)
+        Right rs -> putStrLn $ render (displayRulesets (fromJust $ optNum args) rs)
 
-  where showFile [] = printError
-        showFile (filePath:_) = readFile filePath >>= putStr
+  where
+    parseFile :: [String] -> IO (Either String [(Text, [(Text, Text)])])
+    parseFile (filePath:_) = do contents <- readFile filePath
+                                return $ parseBlocks (pack contents)
 
-        parseFile :: [String] -> IO (Either String [(Text, [(Text, Text)])])
-        parseFile (filePath:_) = do contents <- readFile filePath
-                                    return $ parseBlocks (pack contents)
-
-        printError = putStrLn "Need a file to parse"
-        printParseError error = putStrLn $ "Error parsing css: " ++ error
+    printParseError error = putStrLn $ "Error parsing css: " ++ error
 
 
-displayRulesets :: [RawRuleset] -> Doc
-displayRulesets rawRulesets = do let rulesets = map buildRuleset rawRulesets
-                                     -- redundantRulesets = take 10 $ (nubMatches . groupMatches) (findMatches rulesets)
-                                     -- redundantRulesets = groupMatches (findMatches rulesets)
-                                     redundantRulesets = compactMatches 2 (findMatches rulesets)
-                                 vcat (map format redundantRulesets)
+displayRulesets :: Int -> [RawRuleset] -> Doc
+displayRulesets num rawRulesets = do
+  let rulesets = map buildRuleset rawRulesets
+      redundantRulesets = compactMatches num (findMatches rulesets)
+  vcat (map format redundantRulesets)
   where
     format (ruleset, match) = do let r1 = unpack $ getSelector (snd ruleset)
                                      r2 = unpack $ getMSelector match
