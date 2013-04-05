0d39e3c609b16683f7e324ebce22f650d6c102e2
Zach Moazeni <zach.moazeni@gmail.com>
2013-04-05 14:28:56 -0500

    Cleaning up the RedundancyCalc
    I hope this isn't a bad approach. The function signatures help me grok
    this behavior

--- src/Development/CSSCSS/RedundancyCalc.hs
+++ src/Development/CSSCSS/RedundancyCalc.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Development.CSSCSS.RedundancyCalc where
+module Development.CSSCSS.RedundancyCalc (
+  findMatches
+) where
 
 import Data.Text (Text)
 import Data.List
@@ -8,19 +10,19 @@ import Data.Map (fromListWith, toList)
 
 import Development.CSSCSS.Rulesets
 
-findMatches :: [Ruleset] -> [([Text], [Declaration])]
+type Selector = Text
+type PairedSelectors = ((Selector, Selector), [Declaration])
+type MatchedSelector = ([Selector], [Declaration])
+
+findMatches :: [Ruleset] -> [MatchedSelector]
 findMatches rs = let declarationMap   = fromListWith appendNub (concatMap decTuple rs)
                      pairedMap        = fromListWith appendNub $ concatMap pairSelectors (toList declarationMap)
-                     combinedSeletors = weirdFold doDecsMatch combineSamePairs [] (toList pairedMap)
-                 in sortBy (\(_, decs1) (_, decs2) -> length decs2 `compare` length decs1) combinedSeletors
+                     combinedSeletors = weirdFold doDeclarationsMatch combineSamePairs [] (toList pairedMap)
+                 in sortBy compareDeclarationLengths combinedSeletors
   where
     decTuple (Ruleset selector declarations) = map (\dec -> (dec, [selector])) declarations
     pairSelectors (declaration, selectors)   = map (\pair -> (pair, [declaration])) $ pairs selectors
-
     appendNub a b = nub (a ++ b)
-    doDecsMatch (_, decs1) (_, decs2) = decs1 == decs2
-    combineSamePairs (selectors, decs) passed list = (sort . nub $ (fromTuple selectors) ++ (concatMap (fromTuple . fst) passed), decs):list
-    fromTuple (a, b) = [a, b]
 
 pairs :: [a] -> [(a, a)]
 pairs = concat . pairs'
@@ -35,3 +37,13 @@ weirdFold predicate f initial list = go initial list
     go z []     = z
     go z (x:xs) = let (passList, failList) = partition (predicate x) xs
                   in go (f x passList z) failList
+
+combineSamePairs :: PairedSelectors -> [PairedSelectors] -> [MatchedSelector] -> [MatchedSelector]
+combineSamePairs (selectors, decs) passed xs = (sort . nub $ (fromTuple selectors) ++ (concatMap (fromTuple . fst) passed), decs):xs
+  where fromTuple (a, b) = [a, b]
+
+compareDeclarationLengths :: MatchedSelector -> MatchedSelector -> Ordering
+compareDeclarationLengths (_, decs1) (_, decs2) = length decs2 `compare` length decs1
+
+doDeclarationsMatch :: PairedSelectors -> PairedSelectors -> Bool
+doDeclarationsMatch (_, decs1) (_, decs2) = decs1 == decs2
